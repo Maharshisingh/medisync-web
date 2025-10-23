10887b522292a5183178ec98e846710836232fe3
Maharshisingh <maharshi09singh@gmail.com>
2025-10-24 00:56:31 +0530

    Initial commit - MediSync project

--- medisync-backend/server.js
+++ medisync-backend/server.js
@@ -49,7 +49,7 @@ app.use('/api/admin', adminRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../medisync-frontend/dist')));
   
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../medisync-frontend/dist/index.html'));
   });
 } else {
